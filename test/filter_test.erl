23bd7e9ef93ca7f37f6cbea218be268bdddef85a
Helge Rausch <helge@rausch.io>
2012-12-02 16:19:23 +0100

    fixing tests

--- test/filter_test.erl
+++ test/filter_test.erl
@@ -2,13 +2,36 @@
 -compile(export_all).
 
 before_suite() ->
+	application:start(sasl),
+	application:start(gproc),
 	filter:start().
 
 after_suite() ->
-	filter:stop().
+	filter:stop(),
+	application:stop(gproc),
+	application:stop(sasl).
 
-test_filter_chain() ->
-	Filter = filter:new([1, 1.25], [0.8], self()),
-	test_helper:filter_in(Filter, [0, 0.7, 1, 0.7, 0, -0.7, -1, -0.7]),
-	Out = test_helper:filter_out(Filter, 8, []),
-	[] = Out.
+test_iir_in_combination() ->
+	Filter = filter:new([1], [0.8], self()),
+	test_helper:filter_in(Filter, [1,0,0,0]),
+	Out = test_helper:filter_out(Filter, 4, []),
+	[0, 1.0, -0.8, 0.6400000000000001] = Out.
+
+test_fir_in_combination() ->
+	Filter = filter:new([1,2,3], [0], self()),
+	test_helper:filter_in(Filter, [1,0,0,0]),
+	Out = test_helper:filter_out(Filter, 4, []),
+	[0, 1, 2, 3] = Out.
+
+test_iir_only() ->
+	Filter = filter:new([], [0.8], self()),
+	test_helper:filter_in(Filter, [1,0,0,0]),
+	Out = test_helper:filter_out(Filter, 4, []),
+	[0, 1.0, -0.8, 0.6400000000000001] = Out.
+
+test_fir_only() ->
+	Filter = filter:new([1,2,3], [], self()),
+	test_helper:filter_in(Filter, [1,0,0,0]),
+	Out = test_helper:filter_out(Filter, 4, []),
+	[0, 1, 2, 3] = Out.
+
